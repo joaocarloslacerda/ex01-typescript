3e83ffb0e6edc866c3080da8ea584352387e79cb
joao <juniorlacerdasilva@gmail.com>
2024-05-25 21:13:36 -0300

    Criada parte do exercício com string

--- index.ts
+++ index.ts
@@ -1,1 +1,34 @@
-console.log("teste")
+//string
+interface UserDomain {
+    firstname: string;
+    surname: string;
+    password: string;
+    status: boolean;
+};
+
+const user: UserDomain = {
+    firstname: "João",
+    surname: "Lacerda",
+    password: "teste123",
+    status: true,
+};
+
+function concatenaNome(user: UserDomain): string{
+
+    return `User: ${user.firstname} ${user.surname} \nPassword: ${user.password} \nStatus: ${user.status}`
+}
+console.log(concatenaNome(user))
+
+//number
+
+//Array
+
+//Boolean
+
+//Interface para objetos
+
+//Type para tipos únicos
+
+//Reutilização das interfaces/types
+
+//Faça condicionais com if, ternário com “condição ? true : false” e true || false
