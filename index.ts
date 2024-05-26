0a220b8d02f41c356f2dd2a779945247dda60c3d
joao <juniorlacerdasilva@gmail.com>
2024-05-26 00:20:07 -0300

    Criada parte utilizando number
    Criada parte utilizando array

--- index.ts
+++ index.ts
@@ -16,12 +16,52 @@ const user: UserDomain = {
 function concatenaNome(user: UserDomain): string{
 
     return `User: ${user.firstname} ${user.surname} \nPassword: ${user.password} \nStatus: ${user.status}`
-}
+};
+console.log("--------------------")
 console.log(concatenaNome(user))
 
 //number
+const valueOne: number = 15.00;
+const valueTwo: number = 5;
+
+function multiplyValues(valueOne: number, valueTwo: number): Number{
+
+    return valueOne*valueTwo
+};
+console.log("--------------------")
+console.log(multiplyValues(valueOne, valueTwo))
 
 //Array
+const users: UserDomain[] = [
+    {
+        firstname: "João",
+        surname: "Lacerda",
+        password: "teste123",
+        status: true,
+    },
+    {
+        firstname: "José",
+        surname: "Augusto",
+        password: "123teste",
+        status: false,
+    },
+    {
+        firstname: "Eduardo",
+        surname: "Quadro",
+        password: "123321",
+        status: true,
+    }
+]
+function findUser(users: UserDomain[]):UserDomain[]{
+    
+    const arrayFilter: UserDomain[] = users.filter((user: UserDomain) => user.status == true)
+
+    return arrayFilter
+}
+
+console.log("--------------------")
+console.log(findUser(users))
+
 
 //Boolean
 
